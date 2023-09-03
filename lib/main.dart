5863d8c49eaba322c3fa349f74cf2d180110e475
David RG <davidromerog@gmail.com>
2023-09-03 09:04:36 +0800

    changes on the Scheme color

--- lib/main.dart
+++ lib/main.dart
@@ -8,32 +8,43 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  //Future<ColorScheme> _colorScheme = ColorScheme.fromImageProvider(
+  //    provider: Image.asset('asset/images/GT-Icon-512.png').image);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Gran Tigris Sdn. Bhd.',
+      themeMode: ThemeMode.light,
       theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromARGB(255, 151, 206, 181)),
-          useMaterial3: true,
-          primaryTextTheme: TextTheme(
-            titleLarge: TextStyle(color: Colors.white),
-          )),
+          colorSchemeSeed: const Color.fromARGB(255, 151, 206, 181),
+          //colorSchemeSeed: Colors.teal,
+          //colorScheme: final (ColorScheme.fromImageProvider(provider: Image.asset('asset/images/GT-Icon-512.png').image)),
+          useMaterial3: true),
       home: DefaultTabController(
         length: 3,
+        initialIndex: 0,
         child: Scaffold(
           appBar: AppBar(
-            leading: Builder(
-              builder: (context) => IconButton(
-                icon:
-                    const ImageIcon(AssetImage('asset/images/GT-Icon-512.png')),
-                iconSize: 512,
-                onPressed: () => Null,
+            //leading: Builder(
+            //  builder: (context) => IconButton(
+            //    icon:
+            //        const ImageIcon(AssetImage('asset/images/GT-Icon-512.png')),
+            //    iconSize: 512,
+            //    onPressed: () => Null,
+            //  ),
+            //),
+            leading: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(
+                "asset/images/GT-Icon-512.png",
+                width: 500,
               ),
             ),
+
             title: Text('Gran Tigris Sdn. Bhd.'),
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-            //foregroundColor: Theme.of(context).colorScheme.onPrimary,
+            //backgroundColor: Theme.of(context).colorScheme.primary,
+            //foregroundColor: Theme.of(context).colorScheme.inversePrimary,
             bottom: const TabBar(tabs: [
               Tab(icon: Icon(Icons.home), text: 'Home'),
               Tab(icon: Icon(Icons.info), text: 'About'),
